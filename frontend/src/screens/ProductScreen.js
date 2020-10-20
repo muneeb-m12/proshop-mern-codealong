7fce392da98127262b3c88348adddad8709e9c9b
zambiazzi89 <zambiazzi89@gmail.com>
2020-10-20 16:44:49 -0400

    added Cart reducers

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -8,7 +8,7 @@ import Loader from '../components/Loader'
 import Message from '../components/Message'
 
 const ProductScreen = ({ history, match }) => {
-  const [qty, setQty] = useState(0)
+  const [qty, setQty] = useState(1)
   const dispatch = useDispatch()
 
   const productDetails = useSelector((state) => state.productDetails)
@@ -76,7 +76,7 @@ const ProductScreen = ({ history, match }) => {
                     <Col>
                       <Form.Control
                         as="select"
-                        valute={qty}
+                        value={qty}
                         onChange={(e) => setQty(e.target.value)}
                       >
                         {[...Array(product.countInStock).keys()].map((x) => (
